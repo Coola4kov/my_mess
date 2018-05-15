8a26504172f1ba5935d0df816e4b167f932f460e
coola4kov <light.lds@gmail.com>
2018-05-15 19:33:18 +0500

    Причесал mng_db

--- system/db/mng_db_test.py
+++ system/db/mng_db_test.py
@@ -4,27 +4,6 @@ import time
 import os
 import subprocess
 
-# client = pymongo.MongoClient()
-#
-# db = client.test_database
-# contacts = db.contacts
-#
-# contact = {"name": "test",
-#            "img_base64": '12345678'}
-# contacts.insert_one(contact)
-#
-# print(contacts.find_one({'name': 'test'}))
-# print(client.test_database)
-# post = {"test": "test"}
-# post2 = {"test": "test2", "123": "1234"}
-# collection.insert_one(post)
-# collection.insert_one(post2)
-
-# print(collection.find_one({'test': 'test2'}))
-#
-# import mongoengine
-#
-
 
 class Messages(mongoengine.EmbeddedDocument):
     # False - from contact, True - to contact
@@ -59,19 +38,21 @@ class Db:
         new_msg = Messages(to_from=to_from, datetime=datetime, message=msg)
         Contacts.objects(name=name).update_one(push__msg=new_msg)
 
+    def get_contacts(self):
+        pass
 
 
 if __name__ == '__main__':
-    mongoengine.connect('test')
+    # mongoengine.connect('test')
     # Contacts.drop_collection()
     # Contacts(name='MUSEUN', img_base64=b'123456').save()
     # for element in Contacts.objects:
     #         print(element.name)
     db = Db()
     #
-    db.add_img('MUSEUN', b'1234567890')
+    # db.add_img('MUSEUN', b'1234567890')
     #
-    db.add_msg(to_from=True, name='MUSEUN', datetime=time.ctime(), msg='sdfg, this is test')
+    # db.add_msg(to_from=True, name='MUSEUN', datetime=time.ctime(), msg='sdfg, this is test')
     for element in Contacts.objects:
         for msg in element.msg[-10:]:
             print(msg.datetime, msg.message)
